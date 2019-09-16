aa2d158015e5b6936c363a6b04236aa0dd40d686
Petrisor Neagu <petrisor@madball.ro>
2019-09-16 16:26:57 +0300

    register & some validation

--- core/classes/user.php
+++ core/classes/user.php
@@ -63,4 +63,17 @@ class User{
         header('Location: ../index.php');
     }
 
+    public function checkEmail($email){
+        $stmt = $this->pdo->prepare("SELECT email FROM users WHERE email = :email");
+        $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $count = $stmt->rowCount();
+        if($count > 0){
+            return true;
+        }else{
+            return true;
+        }
+    }
+
 }
--- includes/signup-form.php
+++ includes/signup-form.php
@@ -1,11 +1,40 @@
 <?php
 
+if (isset($_POST['signup'])){
+    $screenName = $_POST['screenName'];
+    $email      = $_POST['email'];
+    $password   = $_POST['password'];
+
+    $error = '';
+    if(empty($screenName) || empty($email) || empty($password)){
+            $error = 'All fields are required!';
+    }else{
+//        email validation
+        $email = $getFromU->checkInput($email);
+        $screenName = $getFromU->checkInput($screenName);
+        $password = $getFromU->checkInput($password);
+         if(!filter_var($email)){
+             $error = "Invalid email format";
+         }else if(strlen($screenName) > 20){
+             $error = 'Name should not be greater than 20 characters.';
+         }else if(strlen($password) < 5){
+             $error = 'Password is too short.';
+         }else{
+             if($getFromU->checkEmail($email) === true){
+                 $error = 'Email is already in use.';
+             }else{
+//                 create the account
+
+             }
+         }
+    }
+}
 
 ?>
 
 <form method="post">
     <div class="signup-div">
-        <h3>Inregistrare </h3>
+        <h3>Register </h3>
         <ul>
             <li>
                 <input type="text" name="screenName" placeholder="Full Name"/>
@@ -20,6 +49,15 @@
                 <input type="submit" name="signup" Value="Inregistreaza-te la Chirip">
             </li>
         </ul>
+<?php
+
+if(isset($error)){
+    echo '<li class="error-li">
+	  <div class="span-fp-error">'.$error.'</div>
+	 </li>';
+}
+?>
+
 
     </div>
 </form>
